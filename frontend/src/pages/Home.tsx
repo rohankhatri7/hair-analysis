66ac5a84ff79f6434a2dff12a4ceab101c810c78
rohankhatri7 <rkhatru0@gmail.com>
2024-11-17 18:09:58 -0800

    updates

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,26 @@
-import Navbar from '../components/Navbar'
-import HeroSection from '../components/HeroSection';
 import { useNavigate } from 'react-router-dom';
+
+import HeroSection from '../components/HeroSection';
+import Navbar from '../components/Navbar';
 import CompaniesSection from '../components/CompaniesSection';
 import PaymentPlansSection from '../components/PaymentPlanSection';
 import Footer from '../components/Footer';
 
 const Home: React.FC = () => {
 
-    const navigate = useNavigate();
-    
+  const navigate = useNavigate();
+
   return (
-    <div className='min-h-screen bg-gray-900'>
-    <Navbar /> 
-    <HeroSection />
-    <PaymentPlansSection />
-    <Footer />
+    <div className= "min-h-screen bg-gray-900">
+     <Navbar />
+     <HeroSection/>
+     <CompaniesSection />
+     <PaymentPlansSection />
+     <Footer />
     </div>
+   
+    
   );
-}
+};
 
 export default Home;
